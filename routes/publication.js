8bdc4b4edc8d108ec882d9cf2ebc3e637d34fb13
Gina Guerrero <gnaguerrer@gmail.com>
2024-01-07 17:33:07 -0500

    feat: routes publications

--- routes/publication.js
+++ routes/publication.js
@@ -1,4 +1,22 @@
 const express = require('express');
 const router = express.Router();
+const middleware = require('../middlewares');
+const PublicationController = require('../controllers/publications');
+
+router.post('/', middleware.auth, PublicationController.createPublication);
+
+router.get(
+  '/detail/:id',
+  middleware.auth,
+  PublicationController.getPublicationById
+);
+router.get(
+  '/user',
+  middleware.auth,
+  PublicationController.getPublicationsByUserId
+);
+router.get('/feed', middleware.auth, PublicationController.getPublications);
+
+router.delete('/:id', middleware.auth, PublicationController.removePublication);
 
 module.exports = router;
